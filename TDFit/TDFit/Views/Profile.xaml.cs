7cb81819c118d542deca57c94bf8e50431cd1684
Bartosz Michalski <bartek10114@wp.pl>
2020-12-23 02:33:55 +0100

    dodanie zmiany hasła i małe poprawki

--- TDFit/TDFit/Views/Profile.xaml.cs
+++ TDFit/TDFit/Views/Profile.xaml.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
-
+using TDFit.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +18,41 @@ namespace TDFit.Views
         public Profile()
         {
             InitializeComponent();
+        }
+
+        private async void OnChangePasswordClicked(object sender, EventArgs e)
+        {
+            var token = Application.Current.Properties["MyToken"].ToString();
+            if (String.IsNullOrWhiteSpace(email.Text) || String.IsNullOrWhiteSpace(newPassword.Text) || String.IsNullOrWhiteSpace(newPassword.Text))
+            {
+                await DisplayAlert("Błąd", "Wypełnij puste pola", "OK");
+                return;
+            }
+                  
+            var model = new ChangePassword
+            {
+                Email = email.Text,
+                Password = oldPassword.Text,
+                newPassword = newPassword.Text,
+            };
+            var json = JsonConvert.SerializeObject(model);
+
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var result = await client.PutAsync("http://192.168.43.212:45455/api/account/ChangePassword", content);
+            if (result.IsSuccessStatusCode)
+            {
+                
+                await DisplayAlert("Sukces", "Zmiana hasła zakończona sukcesem", "OK");
+            }
+            else
+            {
+                
+                await DisplayAlert("Błąd", "Spróbuj ponownie", "OK");
+            }
         }
     }
 }
